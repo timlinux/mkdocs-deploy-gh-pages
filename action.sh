cfaeb7cc91bd756538d0eed6b9f8fc59828c6ca9
Tim Sutton <tim@kartoza.com>
2023-08-07 07:45:37 +0100

    Update action.sh

--- action.sh
+++ action.sh
@@ -53,8 +53,11 @@ fi
 if ! git config --get user.email; then
     git config --global user.email "${GITHUB_ACTOR}@users.noreply.${GITHUB_DOMAIN:-"github.com"}"
 fi
-pip freeze
+
 git remote rm origin
 git remote add origin "${remote_repo}"
-
+echo "----------------------------------------"
+echo " Python Packages Installed"
+echo "----------------------------------------"
+pip freeze
 mkdocs gh-deploy --config-file "${CONFIG_FILE}" --force
